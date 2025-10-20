9f41de06cde36bc19af99a59404fd7f52f89bf5d
aditya5874 <adityagupta5874@gmail.com>
2025-10-21 00:19:23 +0530

    Modify mailSender transport configuration
    Updated mailSender to use port 587 and set secure to false.

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,48 +1,12 @@
-// const nodemailer = require("nodemailer");
-// require("dotenv").config();
 
-// const transporter = nodemailer.createTransport({
-//   host: process.env.MAIL_HOST,
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.MAIL_USER,
-//     pass: process.env.MAIL_PASS,
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-//   logger: true,
-//   debug: true,
-// });
-
-// async function mailSender(email, subject, html) {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"Travia" <${process.env.MAIL_USER}>`,
-//       to: email,
-//       subject: subject,
-//       html: html,
-//     });
-
-//     console.log(" Message sent: %s", info.messageId);
-//     return info;
-//   } catch (error) {
-//     console.error(" Error sending email:", error);
-//     throw error;
-//   }
-// }
-
-// module.exports = mailSender;
-// utils/mailSender.js
 
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
